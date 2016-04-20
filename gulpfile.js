5a70c20c813cea6f8acd379bccb5d1677d404635
Danilo Strazzullo <danilo.strazzullo@gmail.com>
2016-04-21 00:39:59 +0200

    add browser-sync and related gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
 'use-strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
 
+// Gulp config
+const config = {
+  hostname: 'staygold.dev',
+  sassFiles: './sass/**/*.scss'
+}
+
+// Compile sass files
 gulp.task('sass', function () {
- return gulp.src('./sass/**/*.scss')
+ return gulp.src(config.sassFiles)
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
   .pipe(sourcemaps.write('./maps'))
   .pipe(gulp.dest('.'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+// Start server and watch sass files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      proxy: config.hostname
+  });
+
+  gulp.watch(config.sassFiles, ['sass']).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'sass:watch'], function() {
-  // place code for your default task here
-  console.log('I\'m watching...');
-});
+// Default task
+gulp.task('default', ['serve']);
